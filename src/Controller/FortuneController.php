3de69d97a2cc34710b6fd4c0bc1aab1e6214efaa
Habibun Noby <habibun.noby@gmail.com>
2023-06-30 15:13:47 +0600

    13: Filters: Automatically Modify Queries

--- src/Controller/FortuneController.php
+++ src/Controller/FortuneController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CategoryRepository;
 use App\Repository\FortuneCookieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,8 +13,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class FortuneController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(CategoryRepository $categoryRepository, Request $request): Response
+    public function index(CategoryRepository $categoryRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $entityManager->getFilters()
+            ->enable('fortuneCookie_discontinued')
+            ->setParameter('discontinued', false);
+
         $searchTerm = $request->query->get('q');
         if ($searchTerm) {
             $categories = $categoryRepository->search($searchTerm);
